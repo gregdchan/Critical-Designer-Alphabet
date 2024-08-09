3b93fded8934d9144f48ecb2d9621af7e48205e9
Greg D. Chan <gregdchan@gmail.com>
2024-08-09 12:39:27 -0400

    initial commit

---
+++ run-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Navigate to the second package directory and run npm command
+cd designeralphabet
+#npm install
+npm run dev -- --open &
+
+# Navigate to the first package directory and run npm command
+cd ../content
+#npm install
+npm run dev &
+
+# Wait for all background processes to finish
+wait
